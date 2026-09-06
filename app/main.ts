b5ce028502ffdcca133b10768542ffc4e542aff4
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2026-09-07 01:10:23 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -302,15 +302,27 @@ function evaluate(expr: Expr): string | number | boolean | null {
       return left - right;
     }
     if (expr.operator === ">") {
+      if (typeof left !== "number" || typeof right !== "number") {
+        throw new RuntimeError(expr.line, "Operands must be numbers.");
+      }
       return left > right;
     }
     if (expr.operator === ">=") {
+      if (typeof left !== "number" || typeof right !== "number") {
+        throw new RuntimeError(expr.line, "Operands must be numbers.");
+      }
       return left >= right;
     }
     if (expr.operator === "<") {
+      if (typeof left !== "number" || typeof right !== "number") {
+        throw new RuntimeError(expr.line, "Operands must be numbers.");
+      }
       return left < right;
     }
     if (expr.operator === "<=") {
+      if (typeof left !== "number" || typeof right !== "number") {
+        throw new RuntimeError(expr.line, "Operands must be numbers.");
+      }
       return left <= right;
     }
     if (expr.operator === "==") {
